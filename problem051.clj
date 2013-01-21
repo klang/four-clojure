3fe04242056bdfc4570c985ee23faf72e6865eaa
Karsten Lang <k@lang.dk>
2013-01-21 09:35:11 +0100

    source migrated from c130

--- problem051.clj
+++
@@ -1,9 +0,0 @@
-;;Here is an example of some more sophisticated destructuring.
-
-(let [__ "Solution here"]
-  (every? true?
-    [(= [1 2 [3 4 5] [1 2 3 4 5]] (let [[a b & c :as d] __] [a b c d]))]
-    ))
-
-[(= [1 2 [3 4 5] [1 2 3 4 5]]
-    (let [[a b & c :as d] [1 2 3 4 5]] [a b c d]))]
